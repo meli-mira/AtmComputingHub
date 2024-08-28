e20be69ad66ccb5782c5285473dcf34ef6dd3269
Mirabela <medvei.mira@gmail.com>
2024-08-28 22:07:54 +0300

    features added

--- src/app.py
+++ src/app.py
@@ -11,6 +11,16 @@ class KubeClient:
         self.core_client = client.CoreV1Api()
         self.net_client = client.NetworkingV1Api()
 
+    def get_deployment_logs(self, deployment_name):
+        # Get the pods associated with the deployment
+        label_selector = f'app={deployment_name}'
+        pods = self.core_client.list_namespaced_pod(namespace='default', label_selector=label_selector)
+        
+        # Fetch logs from unique pod
+        pod = pods.items[0]
+        logs = self.core_client.read_namespaced_pod_log(name=pod.metadata.name, namespace='default')
+        return logs
+    
     def get_deployment_limits(self, deploy):
         deployment_description = self.apps_client.read_namespaced_deployment(name=deploy.metadata.name, namespace='default')
         container = deployment_description.spec.template.spec.containers[0]
@@ -52,7 +62,7 @@ class KubeClient:
         
         return deployments_descriptions
 
-    def create_workspace(self, deployment_name):
+    def create_workspace(self, deployment_name, cpu, mem, gpu_type, gpu_count):
         assert isinstance(deployment_name, str) and len(deployment_name) > 5
         service_name = f"{deployment_name}-service"
         ingress_name = f"{deployment_name}-ingress"
@@ -76,14 +86,14 @@ class KubeClient:
                                 ports=[client.V1ContainerPort(container_port=8888)],
                                 resources=client.V1ResourceRequirements(
                                     limits={
-                                        "cpu": "16",             # 16 CPU cores
-                                        "memory": "32Gi",        # 32 GB of RAM
-                                        "nvidia.com/gpu": "2"    # 2 GPUs
+                                        "cpu": cpu,             # 16 CPU cores
+                                        "memory": f"{mem}Gi",        # 32 GB of RAM
+                                        "nvidia.com/gpu": gpu_count    # 2 GPUs
                                     }
                                 ),
                             )
                         ],
-                        node_selector={"gpu-type": "k80"}
+                        node_selector={"gpu-type": gpu_type}
                     )
                 )
             )
@@ -176,12 +186,12 @@ kubeClient = KubeClient()
 
 UI_WORKSPACE_SLOTS = 50
 
-def create_workspace(email):
+def create_workspace(cpu, mem, gpu_type, gpu_count, email):
     if not email:
         return "Error: The email field is required."
     email = email.split("@")[0]
     name = email.replace('.', '-')
-    kubeClient.create_workspace(f"workspace-{name}")
+    kubeClient.create_workspace(f"workspace-{name}", cpu, mem, gpu_type.lower(), gpu_count)
     return "Workspace created. Wait 5 minutes and refresh."
 
 def delete_workspace(workspace_name):
@@ -189,13 +199,14 @@ def delete_workspace(workspace_name):
     kubeClient.delete_workspace(workspace_name)
     return "Workspace deleted. Wait 5 minutes and refresh."
 
-# def open_workspace_link(workspace_name):
-#     # Open a URL in a new tab
-#     print(f"{workspace_name}.10.13.0.100.nip.io")
-#     webbrowser.open(f"{workspace_name}.10.13.0.100.nip.io", new=2)
-#     print("gata")
+def get_connection_token(workspace_name):
+    logs = kubeClient.get_deployment_logs(workspace_name)
+    for line in logs.split('\n'):
+        if '8888/lab?token=' in  line:
+            token = line.split('8888/lab?token=')[1]
+            return f'Connect to the workspace using the token: {token}\nToken will be sent via e-mail in the release version.'
+    return 'Could not found token in the logs. Maybe workspace is not ready yet?'
     
-
 def refresh_ui():
     workspaces = [gr.update(value='', visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
     limits_cpu = [gr.update(value='', visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
@@ -203,6 +214,7 @@ def refresh_ui():
     limits_gpu = [gr.update(value='', visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
     status_list = [gr.update(value='', visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
     buttons_launch = [gr.update(visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
+    buttons_token = [gr.update(visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
     buttons_start = [gr.update(visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
     buttons_stop = [gr.update(visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
     buttons_delete = [gr.update(visible=False) for _ in range(UI_WORKSPACE_SLOTS)]
@@ -214,6 +226,7 @@ def refresh_ui():
         limits_mem[i] = gr.update(value=deploy['memory_limit'], visible=True)
         limits_gpu[i] = gr.update(value=deploy['gpu_limit'], visible=True)
         status_list[i] = gr.update(value=deploy['status'], visible=True)
+        buttons_token[i] = gr.update(visible=True)
         buttons_launch[i] = gr.update(visible=True, link=f"http://{deploy['name']}.10.13.0.100.nip.io")
         buttons_start[i] = gr.update(visible=True)
         buttons_stop[i] = gr.update(visible=True)
@@ -221,9 +234,13 @@ def refresh_ui():
         
     status = f"The cluster is running {len(deployments)} workspaces."
     return workspaces + limits_cpu + limits_mem + \
-        limits_gpu + status_list + buttons_launch + \
+        limits_gpu + status_list + buttons_token + buttons_launch + \
         buttons_start + buttons_stop + buttons_delete + [status]
 
+def scaled_markdown(text='', scale=1, **kwargs):
+    with gr.Column(scale=scale, min_width='5%'):
+        md = gr.Markdown(text, **kwargs)
+    return md
 
 with gr.Blocks() as demo:
     gr.Markdown("# ATM Computing Hub")
@@ -233,6 +250,7 @@ with gr.Blocks() as demo:
     workspaces_buttons_stop = []
     workspaces_buttons_delete = []
     workspaces_buttons_launch = []
+    workspaces_buttons_token = []
     workspaces_limits_cpu = []
     workspaces_limits_mem = []
     workspaces_limits_gpu = []
@@ -244,49 +262,58 @@ with gr.Blocks() as demo:
 
     with gr.Accordion("Create new workspace", open=False):
         with gr.Row():
-            create_panel_cpu_count = gr.Number(label="Num CPUs:", value=16)
-            create_panel_mem_count = gr.Number(label="Memory (GB):", value=32)
+            create_panel_cpu_count = gr.Number(label="Num CPUs:", value=12, interactive=True, minimum=1, maximum=32)
+            create_panel_mem_count = gr.Number(label="Memory (GB):", value=24, interactive=True, minimum=1, maximum=64)
             create_panel_gpu_type = gr.Dropdown(label="GPU Type", choices=['K40', 'K80'], value='K80', interactive=True)
-            create_panel_gpu_count = gr.Number(label="Num GPUs:", value=2)
+            create_panel_gpu_count = gr.Number(label="Num GPUs:", value=2, interactive=True, minimum=0, maximum=4)
             create_panel_email = gr.Textbox(label="Email (@mta):", value='')
         create_button = gr.Button("Create Workspace")
         # create_panel_status = gr.Markdown("x")
     
     with gr.Column():
         with gr.Row():
-            gr.Markdown("**Workspaces**")
-            gr.Markdown("**CPU**")
-            gr.Markdown("**Mem**")
-            gr.Markdown("**GPU**")
-            gr.Markdown("**Status**")
-            gr.Markdown("**Actions**")
+            scaled_markdown("**Workspaces**")
+            scaled_markdown("**CPU**")
+            scaled_markdown("**Mem**")
+            scaled_markdown("**GPU**")
+            scaled_markdown("**Status**")
+            scaled_markdown("**Actions**", scale=4)
         
         for i in range(UI_WORKSPACE_SLOTS):
             with gr.Row():
-                workspace_name = gr.Markdown(visible=False)
+                workspace_name = scaled_markdown(visible=False)
                 workspaces.append(workspace_name)
-                workspaces_limits_cpu.append(gr.Markdown(visible=False))
-                workspaces_limits_mem.append(gr.Markdown(visible=False))
-                workspaces_limits_gpu.append(gr.Markdown(visible=False)) # GPU
-                workspaces_status.append(gr.Markdown(visible=False))
-                
-                launch_button = gr.Button("Launch🚀", visible=False)
-                workspaces_buttons_launch.append(launch_button)
-                # launch_button.click(open_workspace_link, inputs=workspace_name)
-                
-                
-                workspaces_buttons_start.append(gr.Button("Start", visible=False))
-                workspaces_buttons_stop.append(gr.Button("Stop", visible=False))
+                workspaces_limits_cpu.append(scaled_markdown(visible=False))
+                workspaces_limits_mem.append(scaled_markdown(visible=False))
+                workspaces_limits_gpu.append(scaled_markdown(visible=False)) # GPU
+                workspaces_status.append(scaled_markdown(visible=False))
 
-                delete_button = gr.Button("Delete", visible=False)
-                workspaces_buttons_delete.append(delete_button)
-                delete_button.click(delete_workspace, inputs=workspace_name, outputs=server_status)
+                with gr.Column(scale=4, min_width='5%'):
+                    with gr.Row():
+                        token_button = gr.Button("Token", visible=False, size='sm')
+                        workspaces_buttons_token.append(token_button)
+                        token_button.click(get_connection_token, inputs=workspace_name, outputs=server_status)
+                        
+                        workspaces_buttons_launch.append(gr.Button("Launch🚀", visible=False, size='sm'))
+                        workspaces_buttons_start.append(gr.Button("Start", visible=False, size='sm'))
+                        workspaces_buttons_stop.append(gr.Button("Stop", visible=False, size='sm'))
+        
+                        delete_button = gr.Button("Delete", visible=False, size='sm')
+                        workspaces_buttons_delete.append(delete_button)
+                        delete_button.click(delete_workspace, inputs=workspace_name, outputs=server_status)
     
     workspaces_outputs = workspaces +  workspaces_limits_cpu + workspaces_limits_mem + \
-        workspaces_limits_gpu + workspaces_status + workspaces_buttons_launch + \
+        workspaces_limits_gpu + workspaces_status + workspaces_buttons_token + workspaces_buttons_launch + \
         workspaces_buttons_start + workspaces_buttons_stop + workspaces_buttons_delete + [server_status]
     refresh_button.click(refresh_ui, outputs=workspaces_outputs)
     
-    create_button.click(create_workspace, inputs=create_panel_email, outputs=server_status)
+    create_button.click(
+        create_workspace, 
+        inputs=[create_panel_cpu_count, 
+                create_panel_mem_count, 
+                create_panel_gpu_type, 
+                create_panel_gpu_count, 
+                create_panel_email], 
+        outputs=server_status)
 
 demo.launch(server_name="0.0.0.0", server_port=8000)
